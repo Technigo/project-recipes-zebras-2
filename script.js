4675e26912212182d17ecf77957f036db7ca1698
Maria Pogosjan <Maria.pogosjan@gmail.com>
2021-02-22 16:52:29 +0100

    JS done

--- script.js
+++ script.js
@@ -1,20 +1,67 @@
 
 
+
 const myRecipeFunc = () => {
-    fetch(
-        `https://api.edamam.com/search?q=vegetarian&app_id=af973ae3&app_key=3d25a0aca50ab9f0f6176749f6525590&from=0&to=12`)
+  fetch(
+    `https://api.edamam.com/search?q=vegetarian&app_id=af973ae3&app_key=3d25a0aca50ab9f0f6176749f6525590&from=0&to=12&time=1%2B`)
     .then((response) => {
-        return response.json()
-    }).then((json) => { 
-        console.log(json)
-        console.log(json.hits[0].recipe.label)
+      return response.json()
+    }).then((json) => {
+
+      const foodTitle = Array.from(
+        json.hits, item => item.recipe.label
+      )
+
+      const foodImage = Array.from(
+        json.hits, item => item.recipe.image
+      )
+
+
+      const foodUrl = Array.from(
+        json.hits, item => item.recipe.url
+      )
+
+
+      const foodRecipe = Array.from(
+        json.hits, item => item.recipe.source
+      )
+
+      const cookingTime = Array.from(
+        json.hits, item => item.recipe.totalTime
+      )
+
+
+      /*const timeInHours = cookingTime.map((value, index) => {
+        return value / 60
+
+        <p>${cookingTime[index]}</p>
+          <p>${timeInHours[index].toFixed(1)} </p>
+      }) */
+
+      const timeConvert = (() => {
+        const num11 = cookingTime
+        const hours = Math.floor( num11/ 60)
+        const minutes = num11 % 60
+        return `${hours}:${minutes}`
+      })
 
 
 
-        const foodTitle = Array.from(
-            json.hits, item => item.recipe.label
-        )
-        console.log(foodTitle)
+      console.log(timeConvert())
+
+
+      const ourRecipes = json.hits
+
+      ourRecipes.forEach((value, index) => {
+        document.getElementById('recipe-items').innerHTML += `
+        <div>
+          <h2>${foodTitle[index]}</h2>
+          <img src="${foodImage[index]}" />
+          <a href="${foodUrl[index]}">${foodRecipe[index]}</a>
+          <p>${cookingTime[index]} minutes</p>
+        </div>
+        `
+      })
     })
 
 }
